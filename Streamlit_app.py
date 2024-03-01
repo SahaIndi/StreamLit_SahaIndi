79fd17047341dcc67caf6c682be7176411ec5dd9
SahaIndi <121751721+SahaIndi@users.noreply.github.com>
2024-03-01 23:58:51 +0800

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -2,7 +2,10 @@ import streamlit as st
 
 def main():
  st.title("Credit Rating forecasting")
-
+ uploaded_file = st.file_uploader("Choose a file")
+ if uploaded_file is not None:
+   df = pd.read_csv(uploaded_file)
+   st.write(dataframe)
 
 if __name__=='__main__':
  main()
