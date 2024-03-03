e69110f07a7114dcf4fb371897090326a1e13589
SahaIndi <121751721+SahaIndi@users.noreply.github.com>
2024-03-04 00:39:53 +0800

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
 import pandas as pd
+import pickle
+
+loaded_model=pickle.load(open('/Users/user/Documents/M.Tech Data Science-BITS Pilani/Fourth Semester/model_pickle','rb'))
+def creditrating_prediction(data):
+    
+    prediction=loaded_model.predict(data)
+    print(prediction)
 
 def main():
  st.title("Credit Rating forecasting")
@@ -7,7 +14,9 @@ def main():
  if uploaded_file is not None:
    df = pd.read_csv(uploaded_file)
    st.write(df)
-
+   st.button('Predict credit rating of the financial institute')
+   st.success(creditrating_prediction)
+  
 if __name__=='__main__':
  main()
  
