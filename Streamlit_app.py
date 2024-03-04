9b952f81446f04da992b92d84f2efc139bb3bcdd
SahaIndi <121751721+SahaIndi@users.noreply.github.com>
2024-03-04 20:30:01 +0800

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -16,8 +16,18 @@ def main():
  if uploaded_file is not None:
    df = pd.read_csv(uploaded_file)
    st.write(df)
-   st.button('Predict credit rating of the financial institute')
-   st.success(creditrating_prediction(df))
+   y_pred=loaded_model.predict(df)
+  
+   y_pred=pd.DataFrame(y_pred,columns=['Predicted-Status'])
+
+   df=df.reset_index(drop=True)
+   new_df=pd.concat([df,y_pred],axis=1,ignore_index=False)
+
+
+#Write the Result to App
+    st.write("Predicted Result") 
+    st.write(new_df)
+    st.success(creditrating_prediction(df))
   
 if __name__=='__main__':
  main()
